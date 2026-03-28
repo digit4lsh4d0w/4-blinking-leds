20abcf43648675b92c5b9668837eb55c828e9d61
Redrov Ivan <digit4lsh4d0w@ya.ru>
2026-03-28 21:59:28 +1000

    Рефакторинг кода, мелкие исправления и документация

--- apps/blinky/src/effects_engine/effect_mode_1.c
+++ apps/blinky/src/effects_engine/effect_mode_1.c
@@ -5,15 +5,25 @@
 #include <stdint.h>
 
 static constexpr uint32_t LED1_PERIOD_MS = 1000;
-static constexpr uint32_t LED1_HALF_PERIOD_MS = LED1_PERIOD_MS / 2;
 static constexpr uint32_t LED2_PERIOD_MS = 500;
-static constexpr uint32_t LED2_HALF_PERIOD_MS = LED2_PERIOD_MS / 2;
 static constexpr uint32_t LED3_PERIOD_MS = 2000;
-static constexpr uint32_t LED3_HALF_PERIOD_MS = LED3_PERIOD_MS / 2;
 static constexpr uint32_t LED4_PERIOD_MS = 1000;
-static constexpr uint32_t LED4_HALF_PERIOD_MS = LED4_PERIOD_MS / 2;
 
 static _Atomic uint8_t mode_shift = 0;
+
+// Функция генерации эффекта "дыхание".
+static inline uint8_t calc_triangle_wave(uint32_t time_ms, uint32_t period,
+                                         uint32_t phase_shift_ms) {
+  uint32_t half_period = period / 2;
+  // Текущее состояние внутри периода волны.
+  uint32_t t = (time_ms + phase_shift_ms) % period;
+
+  if (t < half_period) {
+    return (t * PWM_MAX_BRIGHTNESS) / half_period;
+  } else {
+    return ((period - t) * PWM_MAX_BRIGHTNESS) / half_period;
+  }
+}
 
 void effect_mode_1_short_press() {
   uint8_t shift = atomic_load(&mode_shift);
@@ -23,29 +33,20 @@ void effect_mode_1_short_press() {
 void effect_mode_1_update(uint32_t time_ms) {
   uint8_t func_brightness[LED_COLOR_COUNT];
 
-  func_brightness[0] = (time_ms % LED1_PERIOD_MS < LED1_HALF_PERIOD_MS)
-                           ? PWM_MAX_BRIGHTNESS
-                           : PWM_MIN_BRIGHTNESS;
-  func_brightness[1] = (time_ms % LED2_PERIOD_MS < LED2_HALF_PERIOD_MS)
+  uint32_t led1_half_period = LED1_PERIOD_MS / 2;
+  func_brightness[0] = (time_ms % LED1_PERIOD_MS < led1_half_period)
                            ? PWM_MAX_BRIGHTNESS
                            : PWM_MIN_BRIGHTNESS;
 
-  uint32_t t2 = time_ms % LED3_PERIOD_MS;
-  if (t2 < LED3_HALF_PERIOD_MS) {
-    func_brightness[2] = t2 * PWM_MAX_BRIGHTNESS / LED3_HALF_PERIOD_MS;
-  } else {
-    func_brightness[2] =
-        (LED3_PERIOD_MS - t2) * PWM_MAX_BRIGHTNESS / LED3_HALF_PERIOD_MS;
-  }
+  uint32_t led2_half_period = LED2_PERIOD_MS / 2;
+  func_brightness[1] = (time_ms % LED2_PERIOD_MS < led2_half_period)
+                           ? PWM_MAX_BRIGHTNESS
+                           : PWM_MIN_BRIGHTNESS;
 
-  uint32_t t3 = time_ms % LED4_PERIOD_MS;
-  if (t3 < LED4_HALF_PERIOD_MS) {
-    func_brightness[3] =
-        (LED4_HALF_PERIOD_MS - t3) * PWM_MAX_BRIGHTNESS / LED4_HALF_PERIOD_MS;
-  } else {
-    func_brightness[3] =
-        (t3 - LED4_HALF_PERIOD_MS) * PWM_MAX_BRIGHTNESS / LED4_HALF_PERIOD_MS;
-  }
+  func_brightness[2] = calc_triangle_wave(time_ms, LED3_PERIOD_MS, 0);
+
+  func_brightness[3] =
+      calc_triangle_wave(time_ms, LED4_PERIOD_MS, LED4_PERIOD_MS / 2);
 
   uint8_t shift = atomic_load(&mode_shift);
   for (uint8_t i = 0; i < LED_COLOR_COUNT; i++) {
--- apps/blinky/src/effects_engine/effect_mode_2.c
+++ apps/blinky/src/effects_engine/effect_mode_2.c
@@ -9,7 +9,7 @@ typedef enum : uint8_t {
   EFFECT_MODE_2_SPEED_MID,
   EFFECT_MODE_2_SPEED_HIGH,
   EFFECT_MODE_2_SPEED_COUNT,
-} effect_mode_2_speeds;
+} effect_mode_2_speeds_t;
 
 static constexpr uint32_t speeds_ms[EFFECT_MODE_2_SPEED_COUNT] = {
     [EFFECT_MODE_2_SPEED_LOW] = 1000,
@@ -17,19 +17,20 @@ static constexpr uint32_t speeds_ms[EFFECT_MODE_2_SPEED_COUNT] = {
     [EFFECT_MODE_2_SPEED_HIGH] = 300,
 };
 
-static _Atomic effect_mode_2_speeds current_speed = EFFECT_MODE_2_SPEED_LOW;
+static _Atomic effect_mode_2_speeds_t current_speed = EFFECT_MODE_2_SPEED_LOW;
 static uint32_t last_step_time = 0;
 static uint8_t active_led = 0;
 
 void effect_mode_2_short_press() {
-  effect_mode_2_speeds speed = atomic_load(&current_speed);
+  effect_mode_2_speeds_t speed = atomic_load(&current_speed);
   atomic_store(&current_speed, (speed + 1) % EFFECT_MODE_2_SPEED_COUNT);
 }
 
 void effect_mode_2_update(uint32_t time_ms) {
-  effect_mode_2_speeds speed = atomic_load(&current_speed);
+  effect_mode_2_speeds_t speed = atomic_load(&current_speed);
   uint32_t current_step_duration = time_ms - last_step_time;
 
+  // Устранение визуального бага с скочками свечения
   if (current_step_duration >= speeds_ms[speed] || last_step_time == 0) {
     if (last_step_time != 0) {
       active_led = (active_led + 1) % LED_COLOR_COUNT;
